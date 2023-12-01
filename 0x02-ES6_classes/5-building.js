6a0b040a152f6645fe675cfeaa55ae33c97c4198
nhamidn <hamid-savana@hotmail.fr>
2023-12-01 20:00:57 +0100

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      const properties = Object.getOwnPropertyNames(Building.prototype);
+      if (!properties.find((name) => name === 'evacuationWarningMessage')) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if ((typeof sqft !== 'number') && (sqft instanceof Number)) {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+}
