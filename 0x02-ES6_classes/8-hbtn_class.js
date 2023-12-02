5b560fbfae0b2c59324250baf8e76505ff8ab092
nhamidn <hamid-savana@hotmail.fr>
2023-12-02 01:39:23 +0100

    8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,36 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    this._size = size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    if ((typeof location !== 'string') && !(location instanceof String)) {
+      throw new TypeError('location must be a string');
+    }
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
