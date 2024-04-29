d1545e170e2618302199562cc651f06dd3a6d3c9
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-30 00:01:39 +0100

    update code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-const { stdin: input, stdout: output } = require('node:process');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-output.write('Welcome to Holberton School, what is your name?\n');
-
-input.on('data', (data) => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-input.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
