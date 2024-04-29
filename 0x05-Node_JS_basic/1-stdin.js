66045ead917e644eb39ddf6fdd18773e97794188
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-29 23:58:29 +0100

    update code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
-const rl = readline.createInterface({ input, output });
+output.write('Welcome to Holberton School, what is your name?\n');
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-
-  rl.close();
+input.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 input.on('end', () => {
