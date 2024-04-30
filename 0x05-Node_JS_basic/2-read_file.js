fcd252c0b568cc76d539172334967d87f52abd21
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-30 18:57:11 +0100

    remove try catch

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,37 +8,33 @@ function countStudents(path) {
   if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-    const headers = lines[0].split(',');
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.trim().split('\n');
+  const headers = lines[0].split(',');
 
-    const students = lines.slice(1).map((line) => {
-      const values = line.split(',');
-      const studentObj = {};
-      headers.forEach((header, index) => {
-        studentObj[header] = values[index];
-      });
-      return studentObj;
+  const students = lines.slice(1).map((line) => {
+    const values = line.split(',');
+    const studentObj = {};
+    headers.forEach((header, index) => {
+      studentObj[header] = values[index];
     });
+    return studentObj;
+  });
 
-    const groupedByField = {};
-    students.forEach((student) => {
-      if (!groupedByField[student.field]) {
-        groupedByField[student.field] = [];
-      }
-      groupedByField[student.field].push(student);
-    });
-    console.log(`Number of students: ${students.length}`);
-    Object.keys(groupedByField).forEach((field) => {
-      const names = groupedByField[field].map((student) => student.firstname);
-      console.log(`Number of students in ${field}:`,
-        `${groupedByField[field].length}.`,
-        'List:',
-        names.join(', '));
-    });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  const groupedByField = {};
+  students.forEach((student) => {
+    if (!groupedByField[student.field]) {
+      groupedByField[student.field] = [];
+    }
+    groupedByField[student.field].push(student);
+  });
+  console.log(`Number of students: ${students.length}`);
+  Object.keys(groupedByField).forEach((field) => {
+    const names = groupedByField[field].map((student) => student.firstname);
+    console.log(`Number of students in ${field}:`,
+      `${groupedByField[field].length}.`,
+      'List:',
+      names.join(', '));
+  });
 }
 module.exports = countStudents;
