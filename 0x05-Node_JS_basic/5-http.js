0c52d57823fc037d2cbd23d0684ba5a9f7130c52
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-01 19:41:54 +0100

    Create a more complex HTTP server using Node's HTTP module mandatory

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const fs = require('fs');
+
+const dbPath = process.argv.length > 2 ? process.argv[2] : '';
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const fileLines = data
+        .toString('utf-8')
+        .trim()
+        .split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames
+        .slice(0, dbFieldNames.length - 1);
+
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord
+          .slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
+        }
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
+      }
+
+      const totalStudents = Object
+        .values(studentGroups)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${totalStudents}`);
+      let output = `Number of students: ${totalStudents}`;
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+        output += `\nNumber of students in ${field}: ${group.length}. List: ${studentNames}`;
+      }
+      resolve(output);
+    }
+  });
+});
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(dbPath).then((data) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${data}`);
+    }).catch((err) => {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(err.message);
+    });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
