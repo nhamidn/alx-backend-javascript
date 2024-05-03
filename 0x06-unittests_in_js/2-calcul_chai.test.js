7cd036c4a17b8dce8f9ef135de912390db76be94
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-03 01:29:13 +0100

    2. Basic test using Chai assertion library mandatory

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,35 +1,32 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function() {
   describe('SUM', function() {
     it('should add two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
   });
 
   describe('SUBTRACT', function() {
     it('should subtract two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
   });
 
   describe('DIVIDE', function() {
     it('should divide two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.closeTo(0.2, 0.05);
     });
 
     it('should return "Error" when attempting to divide by zero', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
   });
 
   describe('Edge cases and error handling', function() {
     it('should throw an error for invalid type', function() {
-      assert.throws(() => calculateNumber('INVALID', 2, 3), {
-        name: 'Error',
-        message: 'Invalid type'
-      });
+      expect(() => calculateNumber('INVALID', 2, 3)).to.throw('Invalid type');
     });
   });
 });
