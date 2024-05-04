d980273aef91d64409cd135eba3338fd5989ef00
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-04 02:34:42 +0100

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('Index page', function() {
+  it('should return correct status code', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', function() {
+  it('should return correct status code when :id is a number', function(done) {
+    request('http://localhost:7865/cart/12', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 when :id is not a number', function(done) {
+    request('http://localhost:7865/cart/hello', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
